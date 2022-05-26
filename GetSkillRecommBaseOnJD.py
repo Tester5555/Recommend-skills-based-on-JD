106bb49db8f5e6d028adfd7414a6e412c8d554fd
Sarah <sarah_li5@hotmail.com>
2022-05-26 14:54:08 +1000

    The all skills list support csv file.

--- GetSkillRecommBaseOnJD.py
+++ GetSkillRecommBaseOnJD.py
@@ -1,17 +1,18 @@
-from pathlib import Path
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
+import os.path
+
 
 # This method can extract and save job description to txt file from the given url.
 def test_html_save(job_url, jd_path):
     browser = webdriver.Chrome()
     browser.get(job_url)
-    time.sleep(4) #Waits for 4 secs until the page loads
+    time.sleep(4)
     html_content = browser.page_source  # Getting the html from the webpage
     browser.close()
-    soup = BeautifulSoup(html_content, 'html.parser') # creates a beautiful soup object 'soup'.
+    soup = BeautifulSoup(html_content, 'html.parser')  # creates a beautiful soup object 'soup'.
 
     jds = soup.find_all('li')
 
@@ -19,31 +20,53 @@ def test_html_save(job_url, jd_path):
         for jd in jds:
             jd_file.write(jd.text + '\n')
 
-def search_recommended_skill(skill_list, jd_path, skill_path):
-    # Read all skills and save to a list from existed allSkills file
-    df = pd.read_excel(skill_list, usecols='D')
-    skills_list = df.values.tolist()
 
-    with open(skill_path, 'wt', encoding='utf-8') as skill_file:
-        skill_file.write('Recommended skills for this job:' + '\n')
+def skills_file_to_list(skills_file):
+    # Read all skills and save to a list from existed allSkills file
+    extension = os.path.splitext(skills_file)[1]
+    if extension == '.xlsx' or extension == '.xls':
+        df = pd.read_excel(skills_file, usecols='D')
+    elif extension == '.csv':
+        df = pd.read_csv(skills_file)
+        df.columns = ["Index", "SkillID", "url", "name", "type"]
+    else:
+        print('Skills file only support excel or csv format.')
+
+    skills_list = list(df.name)
+    return skills_list
+
+
+def search_recommended_skill(skills_file, jd_path, recommend_skills_file):
+    # Read all skills and save to a list from existed allSkills file
+  #  df = pd.read_excel(skill_list, usecols='D')
+  #  skills_list = df.values.tolist()
+    skills_list = skills_file_to_list(skills_file)
+
+    with open(recommend_skills_file, 'wt', encoding='utf-8'):
+        recommend_skills_file.write('Recommended skills for this job:' + '\n')
         for skill in skills_list:
+            
             with open(jd_path, 'r') as file:
                 # read all content of a file
                 content = file.read()
                 # check if string present in a file
-                if skill[0] in content:
-                    print(skill[0] + ' is recommended')
-                    skill_file.write(skill[0] + '\n')
+                if skill in content:
+                    print(skill + ' is recommended')
+                    recommend_skills_file.write(skill + '\n')
+
 
 # input the job description webpage here
-jd_url = 'https://www.seek.com.au/job/56898038?type=standard#sol=4038512b16b3c7c94fe23ca8c06996a546765a60'
-all_skills_list = 'C:\Project\Skillojo\jd\\allSkills.xlsx'
-jds_save_path = "C:\Project\Skillojo\jd\jd.txt"
-recommended_skills_path = "C:\Project\Skillojo\jd\\recommended_skills.txt"
+jd_url = 'https://www.seek.com.au/job/57088961?type=standout#sol=b8eb0692125198a860d2c23abae42da821ae67cb'
+# The all skills list be matched.
+all_skills_list = 'C:\Project\Skillojo\jd\\allSkills.csv'
+# Job description will be converted to txt
+jd = "C:\Project\Skillojo\jd\jd.txt"
+# The recommended skills for this job will save in this txt file.
+recommended_skills = "C:\Project\Skillojo\jd\\recommended_skills.txt"
 
 # Scrape job description and save to txt file. This step can be skipped if job description is in txt format.
-test_html_save(jd_url, jds_save_path)
+test_html_save(jd_url, jd)
 
 # Produce recommended skills list based on given job description txt file.
-search_recommended_skill(all_skills_list, jds_save_path, recommended_skills_path)
+search_recommended_skill(all_skills_list, jd, recommended_skills)
 
